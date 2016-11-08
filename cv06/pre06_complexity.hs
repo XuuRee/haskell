6348e62d7f80f4545ad91633cb3863145732d014
XuuRee <filipvalchar@gmail.com>
2016-11-08 18:04:22 +0100

    fibonacci and complexity

--- cv06/pre06_complexity.hs
+++ cv06/pre06_complexity.hs
@@ -12,3 +12,15 @@ reverseListB = rev []
                      rev s (x:t) = rev (x:s) t
 
 -- Algorithm B is better
+
+fib :: Integer -> Integer
+fib = f 0 1
+    where f a _ 0 = a
+          f a b k = f b (a+b) (k-1)
+
+fib2 :: Integer -> Integer
+fib2 0 = 0
+fib2 1 = 1
+fib2 n = fib2 (n-2) + fib2 (n-1)
+
+-- fib2 complexity is exponential, fib is linear -> better!
